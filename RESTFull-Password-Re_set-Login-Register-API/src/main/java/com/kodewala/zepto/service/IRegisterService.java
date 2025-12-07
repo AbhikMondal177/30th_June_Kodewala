731ec8bafabcbb838b2b287710de33515d44f699
Abhik Mondal <110226386+AbhikMondal177@users.noreply.github.com>
2025-12-07 20:43:56 +0530

    Add files via upload

---
+++ RESTFull-Password-Re_set-Login-Register-API/src/main/java/com/kodewala/zepto/service/IRegisterService.java
@@ -0,0 +1,11 @@
+package com.kodewala.zepto.service;
+
+import org.springframework.stereotype.Service;
+
+import com.kodewala.zepto.bean.UserRegisterBean;
+
+@Service
+public interface IRegisterService {
+	
+ String doRegister(UserRegisterBean userRegisterBean);
+}
